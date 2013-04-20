b8772dc5bf79dd5e6b295dd596bc38af1c10f265
Jordi Coscolla <kozko2001@gmail.com>
2013-04-20 06:00:58 +0200

    Initial Commit

---
+++ pillbox/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  pillbox
+//
+//  Created by Jordi Coscolla on 20/04/13.
+//  Copyright (c) 2013 Jordi Coscolla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
